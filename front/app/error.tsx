2e10976969cbc50b0dc5ed89496d1cc245df7955
Salami <nan11556600@gmail.com>
2025-10-20 22:31:43 +0600

    404 + errors done

---
+++ front/app/error.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import Link from "next/link"
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function Error({error, reset}:{
+    error: Error & {digest?: string};
+    reset: () => void;
+}){
+    const router = useRouter();
+
+    useEffect(() => {
+    // Опционально: логирование ошибки
+        console.error(error);
+    }, [error]);
+
+    const goBack = () => {
+        router.back();
+    };
+
+    return (
+        <div className="p_err">
+        <div className="num">
+            <span>Упс</span>
+        </div>
+        <div className="txt">
+            <p>бортовой компьютер перегружен, попробуйте снова</p>
+            <div className="buttons">
+                <button onClick={goBack}>вернуться назад</button>
+                <button onClick={reset}>попробовать снова</button>
+            </div>
+        </div>
+        </div>
+    )
+}
